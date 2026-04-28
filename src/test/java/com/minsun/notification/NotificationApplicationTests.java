6a71ce95760f28e87767e8785435a8270035aaab
minddonyy <minddonyy@gmail.com>
2026-04-28 19:16:57 +0900

    init project

---
+++ src/test/java/com/minsun/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsun.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
